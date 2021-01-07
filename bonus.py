10d77580345f255ec7fe2ae9fdaecd0d566715e3
axmuris <75422834+axmuris@users.noreply.github.com>
2021-01-07 11:34:28 +0100

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -1,18 +1,16 @@
-import random
+from random import randint
 
 class bonus:
-    def__init__(self):
-    self.y=500
-    dir=random.randint(0,1)
-    if dir==1:
-        self.dir=1
-        self.x=0
-    else:
-        self.dir=-1
-        self.x=500
+    def __init__ (self):
+        self.y=20
+        self.exist=0
+        direction=randint(0,1)
+        if direction==1:
+            self.dir=1
+            self.x=-10
+        else:
+            self.dir=-1
+            self.x=1000
 
-    def mouvement(self)
-            self.x=self.x+self.dir
-            if self.x<0 or self.x>500:
-                destroy(self)
-            
+    def mouvement(self):
+            self.x=self.x+self.dir*2
