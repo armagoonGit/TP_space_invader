94cee1dbff3304e6bd1e97e323c100dcf23c444a
axmuris <75422834+axmuris@users.noreply.github.com>
2021-01-07 12:35:07 +0100

    Update bonus.py

--- bonus.py
+++ bonus.py
@@ -3,7 +3,6 @@ from random import randint
 class bonus:
     def __init__ (self):
         self.y=20
-        self.exist=0
         direction=randint(0,1)
         if direction==1:
             self.dir=1
