9468e141a70da9f30075504c3b050ad739ceee23
axmuris <75422834+axmuris@users.noreply.github.com>
2021-01-07 11:33:41 +0100

    Update shelter.py

--- shelter.py
+++ shelter.py
@@ -1,16 +1,5 @@
-from random import randint
+class shelter:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
 
-class bonus:
-    def __init__ (self):
-        self.y=20
-        self.exist=0
-        direction=randint(0,1)
-        if direction==1:
-            self.dir=1
-            self.x=-10
-        else:
-            self.dir=-1
-            self.x=1000
-
-    def mouvement(self):
-            self.x=self.x+self.dir*2
